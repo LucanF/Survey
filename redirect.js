585501956d0abe4c7ff24b873ef8e942c90beaa4
Loki <lucan.florin.marian@gmail.comi>
2024-04-04 13:04:39 +0300

    DSADAS

--- redirect.js
+++ redirect.js
@@ -2,11 +2,8 @@ function validateLogin() {
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
 
-    // Dummy validation for demonstration purposes
     if (username === 'user' && password === 'pass') {
-        // Redirect to the main page
-        console.log("muie");
-        window.location.replace('./main.html');
+        window.location.replace('main.html');
     } else {
         alert('Invalid username or password');
     }
